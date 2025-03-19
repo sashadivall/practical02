e4b018b2b1c1229b06f893f2626ee35ffc0b1cd1
ariellerabin <arielle.rabinovich@gmail.com>
2025-03-19 08:33:53 -0400

    FAISS db--still need to fix context string

--- src/redis/redis_ingest.py
+++ src/redis/redis_ingest.py
@@ -8,7 +8,7 @@ import os
 import fitz
 
 # Initialize Redis connection
-redis_client = redis.Redis(host="localhost", port=6380, db=0)
+redis_client = redis.Redis(host="localhost", port=6379, db=0)
 
 # Specific to this particular embedding model, if we use a different one, have to change this
 VECTOR_DIM = 768
@@ -139,3 +139,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
